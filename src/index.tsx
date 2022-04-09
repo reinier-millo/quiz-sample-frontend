fc27e42bd41c7a65efb81f24c485f6cfef7e99c7
Reinier Millo Sánchez <rmillo@MILLO-DEV>
2022-04-09 00:28:46 -0500

    Update main entry point
    * Update root render to React 18
    * On first load validate user authentication
    
    Signed-off-by: Reinier Millo Sánchez <rmillo@MILLO-DEV>

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { AccountService } from './account/service';
+import FloatNotifications from './notifications/notification';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* Validate is user is authenticated before rendering */
+AccountService.validate().finally(() => {
+  const container: any = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <FloatNotifications></FloatNotifications>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  );
+});
